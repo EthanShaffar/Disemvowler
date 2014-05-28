253b71189f8aab7a9ad7ad05fbc4bd7683a6031e
SeedPaths-User <lessdismissive@email.com>
2014-05-28 10:30:14 -0600

    Upload Data
    Uploading to server. One of my favorite programs! Will update to take
    user input via Console.Readline().
    This upload was needed since current laptop is being returned soon

--- Disemvowler/Program.cs
+++ Disemvowler/Program.cs
@@ -10,6 +10,40 @@ namespace Disemvowler
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Original: Nickleback is my favorite band. Their song writing cant be beat");
+            Console.WriteLine("Disemvowled: " + Disemvowler("Nickleback is my favorite band. Their song writing cant be beat"));
+            Console.WriteLine("Original: How many bears could bear grylls grill if bear grylls could grill bears" );
+            Console.WriteLine("Disemvowled: " + Disemvowler("How many bears could bear grylls grill if bear grylls could grill bears"));
+            Console.WriteLine("Original: I'm a code ninja, baby. I make the functions and make the calls!");
+            Console.WriteLine("Disemvowled: " + Disemvowler("I'm a code ninja, baby. I make the functions and make the calls!"));
+
+            Console.ReadKey();
+        }
+        //This function will take a string of any length and take all vowels (excluding y) and place
+        //them in a separate string while printing both strings out.
+        static string Disemvowler(string inString)
+        {
+            var deVowel = ""; //Placeholder for vowels.
+            var normString = ""; //Placeholder for consonants.
+            for (var i = 0; i < inString.Length; i++)
+            {
+                var Z = inString[i]; //Easy Shortcut for Typing
+
+                //This loop will go through each specific index and confirm with an IF/ELSE statement if its a vowel
+                if (Z == 'a' || Z == 'e' || Z == 'i' || Z == 'o' || Z == 'u')
+                {
+                   //If a vowel,  it will be removed and placed into the deVowel string
+                    
+                    deVowel += Z;
+                    
+                }
+                else
+                {
+                    //If not vowel, it will be returned to string.
+                    normString += Z;
+                }
+            }
+            return normString; //returns the devowled string 
         }
     }
 }
